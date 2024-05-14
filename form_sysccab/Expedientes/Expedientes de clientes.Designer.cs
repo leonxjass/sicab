9a9c84eaeb4a3c18b63d7c0fea89d60f6d177f56
ANDERSON LEON <anderson.leon@mayavision.local>
2024-05-14 12:40:10 -0600

    Inicia el desarrollo del modulo de expedientes, con el modulo de expedientes

---
+++ form_sysccab/Expedientes/Expedientes de clientes.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace form_sysccab_menu.Expedientes
+{
+    partial class Expedientes_de_clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Expedientes_de_clientes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1021, 580);
+            Name = "Expedientes_de_clientes";
+            Text = "Expedientes_de_clientes";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
--- form_sysccab/obj/Release/net8.0-windows/form_sysccab_menu.AssemblyInfo.cs
+++ form_sysccab/obj/Release/net8.0-windows/form_sysccab_menu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("form_sysccab_menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc026117aafd9b71831954696124054cff5d137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab73236ab98937141ace299e62e913f0f8413da")]
 [assembly: System.Reflection.AssemblyProductAttribute("form_sysccab_menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("form_sysccab_menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
